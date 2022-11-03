a3977b05889d4b6ddbc54bd844d496b788c9b472
Daniel Leško <daniel.lesko28@gmail.com>
2022-11-03 16:17:52 +0100

    creating layouts folder and CSS files

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="max-w-[1000px] my-8 mx-auto overflow-hidden py-4 px-8 bg-white">
+    <AppHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import AppHeader from '~/components/AppHeader';
+
+export default {
+    components:{
+        AppHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
