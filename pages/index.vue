765246b9bafedde6849b3913eaf8d08398005ee4
Daniel Leško <daniel.lesko28@gmail.com>
2022-11-03 16:20:18 +0100

    final touches

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial />
+  <div class="flex flex-col items-center">
+    <h1 class="my-2 font-bold text-2xl">Welcome to the best app for jokes</h1>
+    <img src="../assets/funnyPicture.jpg" class="w-[700px] rounded-md my-2" alt="some picture" />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  head(){
+        return {
+            title: 'Welcome to Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for jokes'
+                }
+            ]
+        };
+    },
 }
 </script>
