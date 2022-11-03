9c28be67143ca9da8372ed2b56b0928a546e9c9e
Daniel Leško <daniel.lesko28@gmail.com>
2022-11-03 16:19:39 +0100

    creating pages

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full">
+    <nuxt-link to="/jokes" class="bg-red-400 p-2 rounded-lg m-4 text-white">Back To Jokes</nuxt-link>
+    <h2 class="font-bold my-4 self-center">{{ joke }}</h2>
+    <hr />
+    <small>Joke ID: {{ $route.params.id }}</small>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            joke:{}
+        }
+    },
+    async created(){
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        try{
+            const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config);
+            this.joke = res.data.joke;
+        } catch (err){
+            console.log(err);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-full text-center">
+    <h1 class="font-bold text-2xl">About Jokes</h1>
+    <p class="font-medium text-lg">I made this app for anyone who wants to laugh</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: 'About',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for jokes'
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style>
+
+</style>
